7b43320a1c4ee3ea75ac05d92a17df8fb43fda72
Bekhzodcoder <bexzodcoder@gmail.com>
2025-02-01 15:48:08 +0500

    task

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './navbar';
-export { default as ModalComponent } from './postModal';
-export { default as ModalPutComponent } from './putModal';
-export { default as ModalDeleteComponent } from './deleteModal';
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SignIn } from './sign-in';
-export { default as SignUp } from './sign-up';
-export { default as Main } from './main';
